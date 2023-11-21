878ee0a80793d9c6b3039b5a324e57a6b22bfa8a
aditya maurya <aditya2018m@gmail.com>
2023-11-21 11:28:05 +0530

    salved

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-const assert = require('assert');
+const assert = require("assert");
 
 function countBatteriesByHealth(presentCapacities) {
-  return {
+  const counts = {
     healthy: 0,
     exchange: 0,
-    failed: 0
+    failed: 0,
   };
+
+  presentCapacities.forEach((capacity) => {
+    if (capacity >= 100) {
+      counts["healthy"] += 1;
+    } else if (capacity >= 80) {
+      counts["exchange"] += 1;
+    } else {
+      counts["failed"] += 1;
+    }
+  });
+
+  return counts;
 }
 
 function testBucketingByHealth() {
-  console.log('Counting batteries by SoH...');
+  console.log("Counting batteries by SoH...");
   const presentCapacities = [113, 116, 80, 95, 92, 70];
-  counts = countBatteriesByHealth(presentCapacities);
-  assert(counts["healthy"] == 2);
-  assert(counts["exchange"] == 3);
-  assert(counts["failed"] == 1);
+  const counts = countBatteriesByHealth(presentCapacities);
+  console.log(counts);
+  assert(counts["healthy"] === 2);
+  assert(counts["exchange"] === 3);
+  assert(counts["failed"] === 1);
   console.log("Done counting :)");
 }
 
